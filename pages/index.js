837b24f55ba58edbc6e1d4ff1d72a09a30677c77
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-11-12 10:58:18 -0300

    configurações base para dark/light mode da página

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import config from "../config.json";
 import styled from "styled-components";
-import { CSSReset } from "../src/components/CSSReset";
 import Menu from "../src/components/Menu";
 import { StyledTimeline } from "../src/components/Timeline";
 import React from "react";
@@ -10,7 +9,7 @@ function HomePage() {
 
   return (
     <>
-      <CSSReset />
+      
       <div style={{
         display: "flex",
         flexDirection: "column",
@@ -28,6 +27,8 @@ function HomePage() {
 export default HomePage
 
 const StyledHeader = styled.div`
+  background-color: ${({ theme }) => theme.backgroundLevel1};
+
   img {
     width: 80px;
     height: 80px;
