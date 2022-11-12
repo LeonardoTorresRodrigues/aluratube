32a6d0c619fbfa6d0c010a1845db0f9430ffca2d
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-11-12 12:17:55 -0300

    adicionando funcionalidade de dark/light mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,52 @@
+import React from "react";
 import { ThemeProvider } from "styled-components";
+import ColorModeProvider, { ColorModeContext } from "../src/components/ColorMode";
 import { CSSReset } from "../src/components/CSSReset";
 
 const theme = {
   light: {
-      backgroundBase: "#f9f9f9",
-      backgroundLevel1: "#ffffff",
-      backgroundLevel2: "#f0f0f0",
-      borderBase: "#e5e5e5",
-      textColorBase: "#222222",
+    backgroundBase: "#f9f9f9",
+    backgroundLevel1: "#ffffff",
+    backgroundLevel2: "#f0f0f0",
+    borderBase: "#e5e5e5",
+    textColorBase: "#222222",
   },
   dark: {
-      backgroundBase: "#181818",
-      backgroundLevel1: "#202020",
-      backgroundLevel2: "#313131",
-      borderBase: "#383838",
-      textColorBase: "#FFFFFF",
+    backgroundBase: "#181818",
+    backgroundLevel1: "#202020",
+    backgroundLevel2: "#313131",
+    borderBase: "#383838",
+    textColorBase: "#FFFFFF",
   }
 };
 
 // _app.js -> Definições globais do NextJS
-// ThemeProvider
+// ThemeProvider -> Prover o tema para a app toda
+// ColorModeProvider -> Prove o state de light ou dark mode para todo mundo
 
-export default function MyApp({ Component, pageProps }) {
+function ProviderWrapper(props) {
   return (
-    <ThemeProvider theme={theme.light}>
+    <ColorModeProvider initialMode={"dark"}>
+      {props.children}
+    </ColorModeProvider>
+  )
+}
+
+function MyApp({ Component, pageProps }) {
+  const contexto = React.useContext(ColorModeContext);
+
+  return (
+    <ThemeProvider theme={theme[contexto.mode]}>
       <CSSReset />
       <Component {...pageProps} />
     </ThemeProvider>
+  )
+}
+
+export default function _App(props) {
+  return (
+    <ProviderWrapper>
+      <MyApp {...props} />
+    </ProviderWrapper>
   )
 }
