875a1265ae1a62c00f733375381d34245a9292a4
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-11-16 12:54:57 -0300

    refatorando componente RegisterVideo

--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -1,16 +1,18 @@
-import { createClient } from "@supabase/supabase-js";
-import { useState } from "react";
+import React from "react";
 import { StyledRegisterVideo } from "./styles";
+import { createClient } from "@supabase/supabase-js";
 
+// Whiteboarding
 // Custom Hook
 function useForm(propsDoForm) {
-  const [values, setValues] = useState(propsDoForm.initialValues);
+  const [values, setValues] = React.useState(propsDoForm.initialValues);
 
   return {
     values,
     handleChange: (evento) => {
+      console.log(evento.target);
       const value = evento.target.value;
-      const name = evento.target.name;
+      const name = evento.target.name
       setValues({
         ...values,
         [name]: value,
@@ -19,33 +21,55 @@ function useForm(propsDoForm) {
     clearForm() {
       setValues({});
     }
-  }
-};
+  };
+}
 
 const PROJECT_URL = "https://lbcxxojwabghsjyaqdeh.supabase.co";
-const PUBLICK_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxiY3h4b2p3YWJnaHNqeWFxZGVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njg0MzcwMTksImV4cCI6MTk4NDAxMzAxOX0.9yGzDFu0Q5SaP3x7UG5GClOlR984wamzEX0HSQ69oxo";
-const supabase = createClient(PROJECT_URL, PUBLICK_KEY);
+const PUBLIC_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxiY3h4b2p3YWJnaHNqeWFxZGVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njg0MzcwMTksImV4cCI6MTk4NDAxMzAxOX0.9yGzDFu0Q5SaP3x7UG5GClOlR984wamzEX0HSQ69oxo";
+const supabase = createClient(PROJECT_URL, PUBLIC_KEY);
 
+// get youtube thumbnail from video url
 function getThumbnail(url) {
-  return `https://img.youtube.com/vi${url.split("v=")[1]}/hqdefault.jpg`;
+  return `https://img.youtube.com/vi/${url.split("v=")[1]}/hqdefault.jpg`;
 }
+
+// function getVideoId(url) {
+//     const videoId = url.split("v=")[1];
+//     const ampersandPosition = videoId.indexOf("&");
+//     if (ampersandPosition !== -1) {
+//         return videoId.substring(0, ampersandPosition);
+//     }
+//     return videoId;
+// }
 
 export default function RegisterVideo() {
   const formCadastro = useForm({
-    initialValues: { titulo: "Frost Punk", url: "https://www.youtube.com/watch?v=QsqatJxAUtk" }
+    initialValues: { titulo: "Frost punk", url: "https://www.youtube.com/watch?v=QsqatJxAUtk" }
   });
-  const [formVisivel, setFormVisivel] = useState(false);
+  const [formVisivel, setFormVisivel] = React.useState(false);
+  /*
+  ## O que precisamos para o form funcionar?
+  - pegar os dados, que precisam vir do state
+      - titulo
+      - url do vídeo 
+  - precisamos ter um onSubmit do nosso form
+  - Limpar o formulário após o Submit
+  */
+
   return (
     <StyledRegisterVideo>
       <button className="add-video" onClick={() => setFormVisivel(true)}>
         +
       </button>
+      {/* Ternário */}
+      {/* Operadores de Curto-circuito */}
       {formVisivel
         ? (
           <form onSubmit={(evento) => {
             evento.preventDefault();
+            console.log(formCadastro.values);
 
-            // Contrato entre o nosso Front e o Back-End
+            // Contrato entre o nosso Front e o BackEnd
             supabase.from("video").insert({
               title: formCadastro.values.titulo,
               url: formCadastro.values.url,
@@ -67,7 +91,7 @@ export default function RegisterVideo() {
                 X
               </button>
               <input
-                placeholder="Título do vídeo"
+                placeholder="Titulo do vídeo"
                 name="titulo"
                 value={formCadastro.values.titulo}
                 onChange={formCadastro.handleChange}
@@ -88,3 +112,9 @@ export default function RegisterVideo() {
     </StyledRegisterVideo>
   )
 }
+
+
+// [X] Falta o botão para adicionar
+// [X] Modal
+// -> [X] Precisamos controlar o state
+// -> Formulário em si
