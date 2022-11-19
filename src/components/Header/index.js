d27d45b23080675eb60eee8bbb6a0400cd6e9414
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-11-19 11:52:53 -0300

    create component Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+import { StyledBanner, StyledHeader } from "./styles";
+import config from "../../../config.json";
+
+export function Header() {
+  return (
+    <StyledHeader>
+      <StyledBanner bg={config.bg} />
+      <section className="user-info">
+        <img src={`https://github.com/${config.github}.png`} />
+        <div>
+          <h2>
+            {config.name}
+          </h2>
+          <p>
+            {config.job}
+          </p>
+        </div>
+      </section>
+    </StyledHeader>
+  )
+}
