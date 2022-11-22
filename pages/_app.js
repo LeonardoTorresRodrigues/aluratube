c7fa13d4d20dfd8267cdbdbe4bc31c520dd7dcb6
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-11-22 18:57:11 -0300

    refactoring

--- pages/_app.js
+++ pages/_app.js
@@ -3,23 +3,7 @@ import { ThemeProvider } from "styled-components";
 import { CSSReset } from "../src/components/CSSReset";
 import ColorModeProvider, { ColorModeContext } from "../src/components/Menu/components/ColorMode";
 import RegisterVideo from "../src/components/RegisterVideo";
-
-const theme = {
-  light: {
-    backgroundBase: "#f9f9f9",
-    backgroundLevel1: "#ffffff",
-    backgroundLevel2: "#f0f0f0",
-    borderBase: "#e5e5e5",
-    textColorBase: "#222222",
-  },
-  dark: {
-    backgroundBase: "#181818",
-    backgroundLevel1: "#202020",
-    backgroundLevel2: "#313131",
-    borderBase: "#383838",
-    textColorBase: "#FFFFFF",
-  }
-};
+import { theme } from "../src/common/theme";
 
 // _app.js -> Definições globais do NextJS
 // ThemeProvider -> Prover o tema para a app toda
